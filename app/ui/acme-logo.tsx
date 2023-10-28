da67f20be65216d7417a89dfa9c7d8c0e30f1a77
noman fareed <nomanfareed681@gmail.com>
2023-10-28 15:10:21 +0500

    changes pushed to main

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { sans } from '@/app/ui/fonts';
+import { sans as Sans } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${sans.className} flex flex-row items-center leading-none text-white`}
+      className={`${Sans.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
