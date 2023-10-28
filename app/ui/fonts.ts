68720ded01926e70f3332afe66ad1c72af1e9836
noman fareed <nomanfareed681@gmail.com>
2023-10-28 15:07:52 +0500

    changes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { IBM_Plex_Mono, IBM_Plex_Sans } from 'next/font/google';
+ 
+export const sans = IBM_Plex_Sans({
+  weight: ['400'],
+  subsets: ['latin']
+})
+export const mono = IBM_Plex_Mono({
+    weight: ['400'],
+    subsets: ['latin']
+  })
