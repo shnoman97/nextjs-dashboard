1314add75f1424444655b6af2a6b39edba2915d7
noman fareed <nomanfareed681@gmail.com>
2023-10-28 20:50:49 +0500

    completed

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -8,7 +8,8 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Button } from '@/app/ui/button';
+import { Button } from '@/app/ui/button'
+import { updateInvoice } from '@/app/lib/actions';;
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,7 +19,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   return (
-    <form>
+    <form action={updateInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Invoice ID */}
         <input type="hidden" name="id" value={invoice.id} />
